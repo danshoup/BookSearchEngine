3044a988d2a986749715df1c3008b3e267b60a6e
Daniel Shoup <79939129+danshoup@users.noreply.github.com>
2021-09-18 20:28:39 -0400

    refactored the backend code with typeDefs and resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,83 @@
+// brings in the authentication error from appolo server express
+const { AuthenticationError } = require('apollo-server-express');
+// import user model
+const { Book, User } = require('../models');
+// bring in signToken function from auth file
+const { signToken } = require('../utils/auth.js');
+
+// QUERIES
+// getSingleUser; get single user by id or username
+// MUTATIONS
+// createUser; create a user, sign a token, sent it back
+// login; login user
+// saveBook; save a book to users savedBooks
+// deleteBook; remove a bood from savedBooks
+
+// (reference Unit 21: Activity 26 for syntax context)
+
+const resolvers = {
+
+  Query: {
+
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('savedBooks');
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+
+  },
+
+  Mutation: {
+
+    login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+          throw new AuthenticationError('Login email or password is incorrect!');
+        }
+
+        const correctPw = await user.isCorrectPassword(password);
+
+        if (!correctPw) {
+          throw new AuthenticationError('Login email or password is incorrect!');
+        }
+
+        const token = signToken(user);
+
+        return { token, user };
+    },
+
+
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    saveBook: async (parent, bookData, context) => {
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: bookData } },
+          { new: true, runValidators: true }
+        );
+      } 
+        throw new AuthenticationError('You need to be logged in!');
+    },
+
+    removeBook: async ( parent, { bookId }, context) => {
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: context.user_id },
+          { $pull: { savedBooks: { bookId: bookId } } },
+          { new: true }
+        );
+      }
+        throw new AuthenticationError('You need to be logged in!');
+    }
+
+  },
+};
+
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,32 @@
 const express = require('express');
+
+// Import ApolloServer class
+const { ApolloServer } = require('apollo-server-express');
+
 const path = require('path');
+
+//  Bring in the auth middleware
+const { authMiddleware } = require('./utils/auth');
+
+// Import the two parts of a GraphQL schema
+const { typeDefs, resolvers } = require('./schemas');
+
+//  Bring in the mongodb from connection.js
 const db = require('./config/connection');
-const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Create new instance of Apollo server with GraphQL schema
+const server = new ApolloServer({
+  typeDefs, 
+  resolvers,
+  context: authMiddleware,
+});
+
+// Update Express.js to use Apollo server
+server.applyMiddleware({ app });
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +36,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+ });
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 API server now listening on localhost:${PORT}`);
+    console.log(`GraphQL running at http://localhost:${PORT}${server.grahpqlPath}`);
+  });
 });
