0e8a74ef9d8c501d3319755c3c5bf2de20d07d39
Daniel Shoup <79939129+danshoup@users.noreply.github.com>
2021-09-22 14:09:21 -0400

    adduser token fix

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -40,7 +40,7 @@ const SignupForm = () => {
         variables: {...userFormData}
       });
 
-      Auth.login(data.createUser.token);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
